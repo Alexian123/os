bb8706865093c696dda832ae3bce36f36adcec3f
Alexian Hentiu <alexianhentiu1208@gmail.com>
2026-04-26 21:21:33 +0300

    moved HAL code from main

---
+++ src/kernel/hal.c
@@ -0,0 +1,47 @@
+#include <kernel/vga_io.h>
+#include <kernel/asmcall.h>
+#include <kernel/idt.h>
+#include <kernel/pic_driver.h>
+#include <kernel/isr.h>
+#include <kernel/keyboard_driver.h>
+#include <kernel/input_event_queue.h>
+#include <kernel/tty.h>
+#include <kernel/system_timer.h>
+#include <stdio.h>
+
+static const int system_timer_frequency = 1000; // Hz
+
+void hal_init(void) {
+    debug_printf("[DEBUG] Starting hardware abstraction layer initialization...\n");
+
+    idt_init();
+	debug_printf("[DEBUG]\t- IDT initialized.\n");
+
+	// init PIC
+	const pic_driver_t *pic_driver = get_pic_i8258_driver();
+	pic_driver->remap(PIC_REMAP_OFFSET, PIC_REMAP_OFFSET + 8, false);
+	debug_printf("[DEBUG]\t- %s remapped.\n", pic_driver->name);
+
+	// init input event queue
+	input_queue_init();
+	debug_printf("[DEBUG]\t- Input event queue initialized.\n");
+
+	// init keyboard
+	const keyboard_driver_t *kbd = get_keyboard_driver();
+	kbd->init();
+	debug_printf("[DEBUG]\t- %s driver initialized.\n", kbd->name);
+
+	// init timer
+	system_timer_init(system_timer_frequency);
+	debug_printf("[DEBUG]\t- System timer initialized with %dHz frequency.\n", system_timer_frequency);
+
+	// init TTY
+	tty_init();
+	debug_printf("[DEBUG]\t- TTY initialized.\n");
+
+    // Enable IRQ's
+	sti();
+    debug_printf("[DEBUG]\t- Enabled IRQ's.\n");
+
+    debug_printf("[DEBUG] Finished initializing hardware abstraction layer.\n");
+}
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,44 +1,12 @@
-#include <kernel/vga_io.h>
 #include <kernel/asmcall.h>
-#include <kernel/idt.h>
-#include <kernel/pic_driver.h>
-#include <kernel/isr.h>
-#include <kernel/keyboard_driver.h>
-#include <kernel/input_event_queue.h>
 #include <kernel/tty.h>
 #include <kernel/system_timer.h>
+#include <kernel/hal.h>
 
 #include <stdio.h>
-#include <string.h>
 
 void kernel_main(void) {
-	idt_init();
-	debug_printf("[DEBUG] IDT initialized.\n");
-
-	// init PIC
-	const pic_driver_t *pic_driver = get_pic_i8258_driver();
-	pic_driver->remap(PIC_REMAP_OFFSET, PIC_REMAP_OFFSET + 8, false);
-	debug_printf("[DEBUG] %s remapped.\n", pic_driver->name);
-
-	// init input event queue
-	input_queue_init();
-	debug_printf("[DEBUG] Input event queue initialized.\n");
-
-	// init keyboard
-	const keyboard_driver_t *kbd = get_keyboard_driver();
-	kbd->init();
-	debug_printf("[DEBUG] %s driver initialized.\n", kbd->name);
-
-	// init timer
-	system_timer_init(1000);
-	debug_printf("[DEBUG] System timer initialized.\n", kbd->name);
-
-	// Enable IRQ's
-	sti();
-
-	// init TTY
-	tty_init();
-	debug_printf("[DEBUG] TTY initialized.\n");
+	hal_init();
 
 	// for (;;) {
 	// 	for (int i = 0; i < 5; ++i) {
